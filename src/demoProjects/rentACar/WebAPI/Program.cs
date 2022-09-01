ac23d15875df36c6ef5158d4d2794abf6ee85366
Tahsin020 <84002220+Tahsin020@users.noreply.github.com>
2022-09-01 22:19:25 +0300

    Add Project Files.

--- src/demoProjects/rentACar/WebAPI/Program.cs
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -1,18 +1,8 @@
-using Application;
-using Core.CrossCuttingConcerns.Exceptions;
-using Persistence;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddApplicationServices();
-//builder.Services.AddSecurityServices();
-builder.Services.AddPersistenceServices(builder.Configuration);
-//builder.Services.AddInfrastructureServices();
-//builder.Services.AddHttpContextAccessor();
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -25,9 +15,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-//if (app.Environment.IsProduction())
-    app.ConfigureCustomExceptionMiddleware();
 
 app.UseAuthorization();
 
